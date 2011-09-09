327e476013b093ba4d9e7a56e67b0116cbda9a9c
mmazi <matija.mazi@gmail.com>
2011-09-09 15:50:28 +0200

    minor

--- src/main/java/com/parsek/test/model/Member.java
+++ src/main/java/com/parsek/test/model/Member.java
@@ -25,6 +25,8 @@ public class Member implements Serializable {
     private String phoneNumber;
     private Country country;
 
+    // property accessors
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 

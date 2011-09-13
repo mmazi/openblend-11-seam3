f4e51d981bdf60b488029568334b5e7b3d993198
mmazi <matija.mazi@gmail.com>
2011-09-13 20:48:57 +0200

    revert to JavaEE6 only (no Seam)

--- src/main/java/com/parsek/test/MemberRegistration.java
+++ src/main/java/com/parsek/test/MemberRegistration.java
@@ -1,8 +1,6 @@
 package com.parsek.test;
 
 import com.parsek.test.model.Member;
-import com.parsek.test.seam.MailSender;
-import com.parsek.test.seam.Messages;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
@@ -18,8 +16,6 @@ import javax.persistence.PersistenceContext;
 public class MemberRegistration {
     @PersistenceContext private EntityManager em;
     @Inject private Event<Member> memberEventSrc;
-    @Inject private Messages messages;
-    @Inject private MailSender mailSender;
 
     private Member newMember;
 
@@ -36,14 +32,11 @@ public class MemberRegistration {
     public void register() throws Exception {
         em.persist(newMember);
         memberEventSrc.fire(newMember);
-        mailSender.sayWelcome(newMember);
-        messages.addInfo("member.created", newMember.getName());
         initNewMember();
     }
 
     public void delete(Member m) {
         em.remove(em.getReference(Member.class, m.getId()));
-        messages.addInfo("member.deleted", m.getName());
         memberEventSrc.fire(m);
     }
 }

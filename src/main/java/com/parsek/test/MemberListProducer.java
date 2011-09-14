f3b124997615945465e24cbd8279c72b210bcc2f
mmazi <matija.mazi@gmail.com>
2011-09-14 16:36:10 +0200

    use jpql instead of criteria

--- src/main/java/com/parsek/test/MemberListProducer.java
+++ src/main/java/com/parsek/test/MemberListProducer.java
@@ -7,9 +7,6 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Stateless
@@ -28,11 +25,13 @@ public class MemberListProducer {
 
     @PostConstruct
     public void retrieveAllMembers() {
+/*
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Member> criteria = cb.createQuery(Member.class);
         Root<Member> member = criteria.from(Member.class);
         criteria.select(member).orderBy(cb.asc(member.get("name")));
         members = em.createQuery(criteria).getResultList();
-//        members = em.createQuery("select m from Member m order by m.name", Member.class).getResultList();
+*/
+        members = em.createQuery("select m from Member m order by m.name", Member.class).getResultList();
     }
 }
